615b10c9282c99a385394cff55067dcf2ab5cb85
D3cuca <cucalondaniel@gmail.com>
2021-11-04 09:45:44 +0100

    Initial commit

---
+++ Pile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace MiniVille
+{
+	public class Pile
+	{
+		public List<Card> Cards { get; private set; }
+		public Pile()
+		{
+			this.Cards = new List<Card>();
+		}
+		private void BuildDeck(List<Card> p)
+		{
+			foreach (Utils.CardName cName in Enum.GetValues(typeof(Utils.CardName)))
+			{
+				p.Add(new Card(cName, 6));
+			}
+		}
+	}
+}
+
